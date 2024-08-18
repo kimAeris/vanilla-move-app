b2e646087918a25ea53c7ec40f8616a6dcf569be
KimAeris <kar2125@naver.com>
2024-08-18 17:45:58 +0900

    About 스토어 타이핑

---
+++ README.md
@@ -0,0 +1,4 @@
+## transform tools
+
+JSON 데이터를 기준으로 typeScript의 interface로 바로 변환해주는 사이트  
+JSON.stringify() 문법을 사용하여 JSON 객체로 변환해준 뒤 사용하면 된다.
---
+++ src/store/about.ts
@@ -0,0 +1,18 @@
+import { Store } from "../core/heropy";
+
+interface State {
+  photo: string;
+  name: string;
+  email: string;
+  blog: string;
+  github: string;
+  repository: string;
+}
+export default new Store<State>({
+  photo: "https://heropy.blog/css/images/logo.png",
+  name: "HEROPY / ParkYoungWoong",
+  email: "thesecon@gmail.com",
+  blog: "https://heropy.blog",
+  github: "https://github.com/ParkYoungWoong",
+  repository: "https://github.com/ParkYoungWoong/vanillajs-movie-app",
+});
